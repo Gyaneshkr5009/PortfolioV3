099970b1d74c276f6831de361acbdfe1d7d55354
GYANESH KUMAR <Gyanesh5009@gmail.com>
2025-08-10 23:30:25 +0530

    smoother

--- src/assets/Index.jsx
+++ src/assets/Index.jsx
@@ -4,12 +4,12 @@ import codingNinjas from './codingninjas-svgrepo-com.svg';
 import github from './github.png';
 import linkedIn from './icons8-linkedin-96.png';
 import gfg from './GFG.svg';
-import GA4 from './GA4.png';
-import Naukri from './Naukri.png';
-import Infosys from './infosys.png';
-import Intern from './intern.png';
-import viksit from './Viksit.png';
-import Leetcode from './leetcode.jpg';
+import GA4 from './GA4.webp';
+import Naukri from './Naukri.webp';
+import Infosys from './infosys.webp';
+import Intern from './intern.webp';
+import viksit from './Viksit.webp';
+import Leetcode from './leetcode.webp';
 
 export {
     codeForces,
